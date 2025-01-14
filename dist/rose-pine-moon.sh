1d6ae6c45cd05b57f994cf7b4410e41bb38c777a
mvllow <mvllow@icloud.com>
2025-01-14 17:25:04 -0600

    fix: invalid colour format
    Closes #3

--- dist/rose-pine-moon.sh
+++ dist/rose-pine-moon.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 if [ "$TERM" = "linux" ]; then
 	/bin/echo -e "
-	\e]P0#232136
-	\e]P1#eb6f92
-	\e]P2#9ccfd8
-	\e]P3#f6c177
-	\e]P4#3e8fb0
-	\e]P5#c4a7e7
-	\e]P6#ea9a97
-	\e]P7#e0def4
-	\e]P8#393552
-	\e]P9#eb6f92
-	\e]PA#9ccfd8
-	\e]PB#f6c177
-	\e]PC#3e8fb0
-	\e]PD#c4a7e7
-	\e]PE#ea9a97
-	\e]PF#e0def4
+	\e]P0232136
+	\e]P1eb6f92
+	\e]P29ccfd8
+	\e]P3f6c177
+	\e]P43e8fb0
+	\e]P5c4a7e7
+	\e]P6ea9a97
+	\e]P7e0def4
+	\e]P8393552
+	\e]P9eb6f92
+	\e]PA9ccfd8
+	\e]PBf6c177
+	\e]PC3e8fb0
+	\e]PDc4a7e7
+	\e]PEea9a97
+	\e]PFe0def4
 	"
 	# get rid of artifacts
 	clear
--- dist/rose-pine.sh
+++ dist/rose-pine.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 if [ "$TERM" = "linux" ]; then
 	/bin/echo -e "
-	\e]P0#191724
-	\e]P1#eb6f92
-	\e]P2#9ccfd8
-	\e]P3#f6c177
-	\e]P4#31748f
-	\e]P5#c4a7e7
-	\e]P6#ebbcba
-	\e]P7#e0def4
-	\e]P8#26233a
-	\e]P9#eb6f92
-	\e]PA#9ccfd8
-	\e]PB#f6c177
-	\e]PC#31748f
-	\e]PD#c4a7e7
-	\e]PE#ebbcba
-	\e]PF#e0def4
+	\e]P0191724
+	\e]P1eb6f92
+	\e]P29ccfd8
+	\e]P3f6c177
+	\e]P431748f
+	\e]P5c4a7e7
+	\e]P6ebbcba
+	\e]P7e0def4
+	\e]P826233a
+	\e]P9eb6f92
+	\e]PA9ccfd8
+	\e]PBf6c177
+	\e]PC31748f
+	\e]PDc4a7e7
+	\e]PEebbcba
+	\e]PFe0def4
 	"
 	# get rid of artifacts
 	clear
